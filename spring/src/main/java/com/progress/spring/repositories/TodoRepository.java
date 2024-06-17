b5ada88d3b2210884a1fc51402ea4158536c0afe
Sakthikarthick <sakthikarthick3107@gmail.com>
2024-06-17 18:22:13 +0530

    WorkflowX

--- spring/src/main/java/com/progress/spring/repositories/TodoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.progress.spring.repositories;
-
-import com.progress.spring.models.Todo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TodoRepository extends JpaRepository<Todo,Integer> {
-}
